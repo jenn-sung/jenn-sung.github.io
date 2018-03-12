829b8c57a944c625a61fc41510b0530e3dc18d08
JennSung <jennifer.sung@colorado.edu>
2018-03-12 01:10:58 -0600

    Add homepage, portfolio, blog, about, and contact vue templates and components.

--- index.js
+++ index.js
@@ -12,6 +12,78 @@ var HomePage = {
   computed: {}
 };
 
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
+
+// End Homepage component
+
+// Begin About Page component
+
+var AboutPage = {
+  template: "#about-page",
+  data: function() {
+    return {
+      message2: "about!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+// End About Page component
+
+// Begin Portfolio Page component
+
+var HomePage = {
+  template: "#portfolio-page",
+  data: function() {
+    return {
+      message3: "portfolio"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+// End Portfolio Page component
+
+// Begin Blog component
+
+var HomePage = {
+  template: "#blog-page",
+  data: function() {
+    return {
+      message4: "Blog"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+// End Blog Component
+
+// Begin Contact Component
+
+var HomePage = {
+  template: "#contact-page",
+  data: function() {
+    return {
+      message: "contact"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+// End Contact component
+
+
 var router = new VueRouter({
   routes: [{ path: "/", component: HomePage }],
   scrollBehavior: function(to, from, savedPosition) {
@@ -23,3 +95,5 @@ var app = new Vue({
   el: "#vue-app",
   router: router
 });
+
+
